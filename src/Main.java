addb03684090959fd3b2680ee91a5f6437a9d6c0
Jose wellison de Sousa <wellison_designer@hotmail.com>
2025-07-07 17:20:56 -0300

    Criando as funçoes de calculo basico

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
+
+    public static double somar(double a, double b){
+        return a + b;
+    }
+    public static double subtrair(double a, double b){
+        return a - b;
+    }
+    public static double multiplicar(double a, double b){
+        return a * b;
+    }
+    public static double dividir(double a, double b){
+        //cuidado: nao pode dividir por 0
+        if(b==0){
+            //lançar um erro para a execução do codigo, por isso é melhor usar, NaN.
+            //throw new ArithmeticException("Divisão por zero");
+
+            System.out.println("Erro: divisão por zero");
+            return  Double.NaN;
+        }
+        return a / b;
+    }
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
